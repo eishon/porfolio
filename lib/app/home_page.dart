48b619e55ea33297265bf4aae1a5d1828e2cf825
Ahmed Fahim Foysal Eishon <ahmed-fahim-foysal-eishon@ib-system.co.jp>
2021-08-27 17:01:51 +0900

    parallax example

--- lib/app/home_page.dart
+++ lib/app/home_page.dart
@@ -1,49 +1,57 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
+import 'ui/example_parallax.dart';
+
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Size _screenSize = MediaQuery.of(context).size;
 
     return Scaffold(
-      body: Container(
-        width: double.infinity,
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              Container(
-                height: 480,
-                child: Stack(
-                  alignment: AlignmentDirectional.center,
-                  children: [
-                    Image.network(
-                      'https://picsum.photos/seed/picsum/${_screenSize.width}/360',
-                    ),
-                    Align(
-                      alignment: Alignment.bottomCenter,
-                      child: Container(
-                        width: 200,
-                        child: CircleAvatar(
-                          radius: 100,
-                          backgroundImage: ExactAssetImage('assets/pic.png'),
-                        ),
-                        decoration: BoxDecoration(
-                          shape: BoxShape.circle,
-                          border: Border.all(
-                            color: Colors.white,
-                            width: 4.0,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              )
-            ],
-          ),
-        ),
+      body: Center(
+        child: ExampleParallax(),
       ),
     );
+
+    // return Scaffold(
+    //   body: Container(
+    //     width: double.infinity,
+    //     child: SingleChildScrollView(
+    //       child: Column(
+    //         children: [
+    //           Container(
+    //             height: 480,
+    //             child: Stack(
+    //               alignment: AlignmentDirectional.center,
+    //               children: [
+    //                 Image.network(
+    //                   'https://picsum.photos/seed/picsum/${_screenSize.width}/360',
+    //                 ),
+    //                 Align(
+    //                   alignment: Alignment.bottomCenter,
+    //                   child: Container(
+    //                     width: 200,
+    //                     child: CircleAvatar(
+    //                       radius: 100,
+    //                       backgroundImage: ExactAssetImage('assets/pic.png'),
+    //                     ),
+    //                     decoration: BoxDecoration(
+    //                       shape: BoxShape.circle,
+    //                       border: Border.all(
+    //                         color: Colors.white,
+    //                         width: 4.0,
+    //                       ),
+    //                     ),
+    //                   ),
+    //                 ),
+    //               ],
+    //             ),
+    //           )
+    //         ],
+    //       ),
+    //     ),
+    //   ),
+    // );
   }
 }
